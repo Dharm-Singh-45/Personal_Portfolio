b3e1ad6d473df6b6d7599c7a8f19862221bc63f8
Dharm-Singh-45 <dharmjat45@gmail.com>
2024-11-13 11:21:34 +0530

    user id update

--- dashboard/dashboard/src/pages/sub-components/Dashboard.jsx
+++ dashboard/dashboard/src/pages/sub-components/Dashboard.jsx
@@ -55,7 +55,7 @@ const Dashboard = () => {
   return (
     <>
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-2 sl:grid-cols-2 w-screen" >
+        <main className="flex flex-wrap justify-center items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:w-full xl:w-full w-screen" >
           <div className='"grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2"'>
             <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
               <Card className="sm:col col-span-2">
